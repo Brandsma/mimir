755b011134e70e39a76aa57178dc73e4090925cf
bartelsh <bartelsh@live.nl>
2022-03-03 13:15:10 -0600

    added question generation

---
+++ question_generation.py
@@ -0,0 +1,15 @@
+from logger import setup_logger
+from transformers import pipeline
+
+log = setup_logger(__name__)
+
+
+def generate_question(context):
+    if not isinstance(context, str): log.error("context is not a string.")
+    qgen = pipeline(model = "mrm8488/t5-base-finetuned-question-generation-ap")
+    return qgen("context: " + context)[0]['generated_text']
+
+
+if __name__ == "__main__":
+    context = "Cows can weigh up to 700 kilograms."
+    log.info(generate_question(context))
