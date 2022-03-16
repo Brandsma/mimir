b38d6bb13fe2db8b6fdd19f99f94e40353043b33
IvoSte <36919251+IvoSte@users.noreply.github.com>
2022-03-16 15:32:35 +0100

    Analyze contexts

--- main.py
+++ main.py
@@ -5,18 +5,31 @@ from logger import setup_logger
 
 log = setup_logger(__name__)
 
+def analyze_contexts(contexts):
+    print("--- Contexts")
+    print(contexts)
+    print("--- tpyes")
+    print([type(x) for x in contexts])
+    print("--- lens")
+    print([len(x) for x in contexts])
+
 def answer_question_pipeline(question):
     # Input (raw) source text
     # Normal
     source = data.retrieve_paragraphs()
-    contexts = [x for x in source['train']['text'] if len(x) < 512]
+    contexts = [x for x in source['train']['text']]
     # FAISS
     #contexts = data.retrieve_paragraphs()
     #print(contexts)
 
+    analyze_contexts(contexts)
+    exit()
+
     # retrieve context from source text based on question
     context = retrieve_context(question, contexts, n=5)
-    context = [x[0] for x in context]
+    print(context)
+    exit()
+    #context = [x[0] for x in context]
 
     # Answer question with context
     answer = retrieve_answer(question, context, language='en', num_generated_answers=5, num_correct_answers_required=3)
