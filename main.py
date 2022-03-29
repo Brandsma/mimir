0a53928053a1e820cee75b606f3abe78c4e6b471
abe-manjaro-desktop <a.brandsma@protonmail.com>
2022-03-29 15:58:43 +0200

    Added context score

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from util.logger import setup_logger
 log = setup_logger(__name__)
 
 
-def main():
+def evaluate_answering_pipeline():
     ## SETUP ##
     # Setup objects with their config
     data = Data_pipeline()
@@ -23,7 +23,9 @@ def main():
     # TODO: Human input
     questions = io.get_all_questions()
     answers = io.get_all_answers()
+    paragraphs_related_to_answers = io.get_all_paragraphs_for_answers()
     answer_embeddings = embedding_creator.encode(answers)
+    # TODO: Challenge, better clean up for the paragraphs
     context_list = io.get_paragraphs()
 
     try:
@@ -34,12 +36,18 @@ def main():
                 )
                 result_objects = give_answer_to_question_pipeline(
                     question, context_list)
+
                 result_objects["true_answer"] = answers[idx]
+
                 result_objects[
                     "true_answer_similarity_score"] = util.dot_score(
                         embedding_creator.encode(
                             result_objects["translated_answer"]),
                         answer_embeddings[idx])
+
+                result_objects["true_context_similarity_score"] = util.dot_score(embedding_creator.encode(
+                    paragraphs_related_to_answers[idx]), embedding_creator.encode(result_objects["most_relevant_contexts"][0]))
+                
                 data.add_qa_results(result_objects)
                 io.print_results(result_objects)
     except KeyboardInterrupt:
@@ -48,7 +56,7 @@ def main():
         return
     df = data.to_df()
     df.to_csv("./first_run.csv")
-    #generate_questions_pipeline()
+    # generate_questions_pipeline()
     # summarize_text_pipeline()
 
 
